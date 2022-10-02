bf979b3a956cbbe7c045e5657b4898988c262a4c
DESKTOP-8HRPD85\aclie <sjs939883@gmail.com>
2022-10-02 18:01:09 +0900

    주소 추가 시 생성시간 자동 추가

---
+++ src/main/java/com/example/shorturl/domain/UrlInfo.java
@@ -0,0 +1,38 @@
+package com.example.shorturl.domain;
+
+import com.example.shorturl.Timestamped;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "urlInfo")
+public class UrlInfo extends Timestamped {
+
+    @Id
+    @Column(name = "urlInfo_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //진짜주소
+    @Lob
+    private String realUrl;
+
+    //만든 주소
+    private String fakeUrl;
+
+    //방문자수
+    private Long visitCount;
+
+    @Builder
+    public UrlInfo(String realUrl, String fakeUrl, Long visitCount) {
+        this.realUrl = realUrl;
+        this.fakeUrl = fakeUrl;
+        this.visitCount = visitCount;
+    }
+}
