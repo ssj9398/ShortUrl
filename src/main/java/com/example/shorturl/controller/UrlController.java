4bc212da40da2ae219f1670c494cb52888daffd6
DESKTOP-8HRPD85\aclie <sjs939883@gmail.com>
2022-10-13 01:35:49 +0900

    [STYLE] 리턴 메세지 수정

--- src/main/java/com/example/shorturl/controller/UrlController.java
+++ src/main/java/com/example/shorturl/controller/UrlController.java
@@ -27,7 +27,7 @@ public class UrlController {
 
     @GetMapping("url")
     public ResponseEntity<Success> getTopTenUrlList(){
-        return new ResponseEntity<>(new Success("최근 등록 된 주소 10개 등록 성공!",urlService.getTopTenUrlList()),HttpStatus.OK);
+        return new ResponseEntity<>(new Success("최근 등록 된 주소 10개 조회 성공!",urlService.getTopTenUrlList()),HttpStatus.OK);
     }
 
     @GetMapping("url/{url}")
