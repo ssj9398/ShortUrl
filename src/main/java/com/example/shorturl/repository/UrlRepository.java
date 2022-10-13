6645ff428d8840ecb407128419d6007af16841cd
DESKTOP-8HRPD85\aclie <sjs939883@gmail.com>
2022-10-14 02:04:19 +0900

    [FEAT] 최근 등록된 url 10개 조회 시 공개 설정만 조회 가능하게 조건 수정

--- src/main/java/com/example/shorturl/repository/UrlRepository.java
+++ src/main/java/com/example/shorturl/repository/UrlRepository.java
@@ -2,6 +2,7 @@ package com.example.shorturl.repository;
 
 import com.example.shorturl.domain.UrlInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +10,5 @@ import java.util.Optional;
 public interface UrlRepository extends JpaRepository<UrlInfo, Long> {
     Optional<UrlInfo> findByFakeUrl(String url);
 
-    List<UrlInfo> findTop10ByOrderByCreatedAtDesc();
+    List<UrlInfo> findTop10ByOpenStatusOrderByCreatedAtDesc(boolean openStatus);
 }
