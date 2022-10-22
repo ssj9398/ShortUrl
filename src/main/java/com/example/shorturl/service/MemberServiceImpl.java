d4d0b51318f98b0ba2fa80f0eb4183adc51e6f27
alisyabob <sjs939883@gmail.com>
2022-10-23 04:01:01 +0900

    [FEAT] 회원가입 예외처리 추가

--- src/main/java/com/example/shorturl/service/MemberServiceImpl.java
+++ src/main/java/com/example/shorturl/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.shorturl.service;
 
+import com.example.shorturl.advice.exception.ApiRequestException;
 import com.example.shorturl.domain.Member;
 import com.example.shorturl.dto.MemberRequestDto;
 import com.example.shorturl.repository.MemberRepository;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -15,11 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
-    
+
     @Override
     @Transactional
     public void addMember(MemberRequestDto.Create create) {
-        Member member = Member.createMember(create.getEmail(), create.getPassword());
-        memberRepository.save(member);
+        Optional<Member> optMember = memberRepository.findByEmail(create.getEmail());
+        if(optMember.isEmpty()){
+            Member member = Member.createMember(create.getEmail(), create.getPassword());
+            memberRepository.save(member);
+        }else {
+            throw  new ApiRequestException("이미 존재하는 회원입니다.");
+        }
+
     }
 }
