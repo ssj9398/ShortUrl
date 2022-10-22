b345a0e570a3ecc780dc4130da8215c5fce6035e
alisyabob <sjs939883@gmail.com>
2022-10-23 04:04:17 +0900

    [FEAT] 로그인 페이지 추가

--- src/main/java/com/example/shorturl/controller/MemberController.java
+++ src/main/java/com/example/shorturl/controller/MemberController.java
@@ -6,6 +6,7 @@ import com.example.shorturl.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,10 @@ public class MemberController {
     public ResponseEntity<Success> addMember(@RequestBody MemberRequestDto.Create create){
         memberService.addMember(create);
         return new ResponseEntity<>(new Success("회원가입 완료",""), HttpStatus.CREATED);
+    }
+
+    @GetMapping("login")
+    public String login(){
+        return "login";
     }
 }
