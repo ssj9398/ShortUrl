73df402ae1187baa560f94d4dc9f953da2d34472
alisyabob <sjs939883@gmail.com>
2022-10-23 03:04:21 +0900

    [FEAT] 사용자 테이블 추가

---
+++ src/main/java/com/example/shorturl/domain/Member.java
@@ -0,0 +1,25 @@
+package com.example.shorturl.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
