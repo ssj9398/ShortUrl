e36e07042be7d75af28b6980db8f82b1ac2000c3
alisyabob <sjs939883@gmail.com>
2022-10-26 01:36:05 +0900

    [FEAT] 비밀번호 암호화 저장 및 로그인 시 비밀번호 체크로직 수정

--- src/main/java/com/example/shorturl/service/MemberServiceImpl.java
+++ src/main/java/com/example/shorturl/service/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.shorturl.advice.exception.ApiRequestException;
 import com.example.shorturl.domain.Member;
 import com.example.shorturl.dto.MemberRequestDto;
 import com.example.shorturl.repository.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,14 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     @Transactional
     public void addMember(MemberRequestDto.Create create) {
         Optional<Member> optMember = memberRepository.findByEmail(create.getEmail());
         if(optMember.isEmpty()){
-            Member member = Member.createMember(create.getEmail(), create.getPassword());
+            Member member = Member.createMember(create.getEmail(), passwordEncoder.encode(create.getPassword()));
             memberRepository.save(member);
         }else {
             throw  new ApiRequestException("이미 존재하는 회원입니다.");
@@ -34,9 +37,11 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public void loginMember(MemberRequestDto.Login login) {
-        Optional<Member> optMemberByEmailAndPassword = memberRepository.findByEmailAndPassword(login.getEmail(), login.getPassword());
-        if(optMemberByEmailAndPassword.isEmpty()){
+        Optional<Member> optMember = memberRepository.findByEmail(login.getEmail());
+        if(optMember.isEmpty()){
             throw new ApiRequestException("존재하지 않는 회원입니다.");
+        } else if (!passwordEncoder.matches(login.getPassword(), optMember.get().getPassword())) {
+            throw new ApiRequestException("비밀번호 틀림");
         }
     }
 }
