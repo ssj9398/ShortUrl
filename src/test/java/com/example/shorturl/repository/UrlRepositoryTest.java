47371cf58faf36462329025ac1dc0612060db696
DESKTOP-B76L9MB\User <sjs939883@gmail.com>
2022-11-02 09:45:27 +0900

    [TEST] repository 테스트 가짜주소(미로그인)로찾기

--- src/test/java/com/example/shorturl/repository/UrlRepositoryTest.java
+++ src/test/java/com/example/shorturl/repository/UrlRepositoryTest.java
@@ -29,6 +29,26 @@ class UrlRepositoryTest {
         assertThat(saveUrlInfo.getFakeUrl()).isEqualTo(urlInfo.getFakeUrl());
         assertThat(saveUrlInfo.getRealUrl()).isEqualTo(urlInfo.getRealUrl());
         assertThat(saveUrlInfo.isOpenStatus()).isEqualTo(urlInfo.isOpenStatus());
+    }
 
+    @Test
+    void 미로그인_가짜주소로찾기(){
+        //given
+        UrlInfo urlInfo = UrlInfo.builder()
+                .realUrl("google.com")
+                .fakeUrl("localhost/abcdef")
+                .openStatus(true)
+                .build();
+        UrlInfo saveUrlInfo = urlRepository.save(urlInfo);
+
+        //when
+        UrlInfo url = urlRepository.findByFakeUrl("localhost/abcdef").get();
+
+        //then
+        assertThat(url.getId()).isEqualTo(saveUrlInfo.getId());
+        assertThat(url.getFakeUrl()).isEqualTo(urlInfo.getFakeUrl());
+        assertThat(url.getRealUrl()).isEqualTo(urlInfo.getRealUrl());
+        assertThat(url.getCreatedAt()).isEqualTo(urlInfo.getCreatedAt());
+        assertThat(url.getVisitCount()).isEqualTo(urlInfo.getVisitCount());
     }
 }
